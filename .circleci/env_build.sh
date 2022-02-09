1389f18bf16117b36dfa4598668316e0cef6492e
Mojtaba Samimi <arch.mojtaba.samimi@gmail.com>
2022-02-09 10:47:34 -0500

    alias for python so that node-gyp work

--- .circleci/env_build.sh
+++ .circleci/env_build.sh
@@ -2,5 +2,6 @@
 export NODE_OPTIONS='--max-old-space-size=4096' && \
 echo "node version: $(node --version)" && \
 echo "npm version: $(npm --version)" && \
+alias python=$(which python3) && \
 npm ci && \
 npm ls --prod --all
